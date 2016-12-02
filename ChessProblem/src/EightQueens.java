bd9de51f09d72561127c110b4bf95a27c8443dd2
Sam Korman <skorman101@gmail.com>
2016-12-01 18:37:37 -0700

    Completely Works! v0.3

--- ChessProblem/src/EightQueens.java
+++ ChessProblem/src/EightQueens.java
@@ -9,27 +9,13 @@ public class EightQueens {
 	private int[][] queenPositions = new int[8][2];
 	private ArrayList<Integer> temp = new ArrayList<Integer>();
 	private ArrayList<int[][]> solutions = new ArrayList<int[][]>();
-	private ArrayList<int[][]> things = new ArrayList<int[][]>();
-	
-	
+	private ArrayList<int[][]> nonSolutions = new ArrayList<int[][]>();
 	
 	public void getEightQueens(){
 		
 		for (int i = 0; i < 8; i++){
 			queenPositions[i][1] = i;
 		}
-		
-		int[][] num = {
-				{2, 0},
-				{6, 1},
-				{5, 2}, 
-				{4, 3},
-				{3, 4},
-				{2, 5},
-				{1, 6},
-				{0, 7}
-		};
-		//System.out.println(isSolution(num));
 		
 		for(int first = 0; first < 8; first++){
 			if(!temp.contains((Integer) first)){
@@ -78,15 +64,10 @@ public class EightQueens {
 																		queenPositions[6][0] = seventh;
 																		queenPositions[7][0] = eigth;
 																		
-																		if (isSolution(queenPositions)){
-																			//print(queenPositions);
-																			//System.out.println("temp: " + temp);
-																			//System.out.println();
-																			//solutions.add(copyArray(queenPositions));
-																			//print(solutions.get(solutions.size() - 1));
-																			//System.out.println();
-																			//System.out.println(solutions.size());
-																			//drawSolution(queenPositions);
+																		if (isSolution(queenPositions) && !isSolutionChosenBefore(queenPositions)){
+																			solutions.add(queenPositions);
+																			createNonAnswers(queenPositions);
+
 																		}
 																		
 																		temp.remove(temp.size() - 1);
@@ -113,37 +94,24 @@ public class EightQueens {
 				temp.remove(temp.size() - 1);
 			}
 		}
-		//things.add(num);
-				
+		
 		for(int i = 0; i < solutions.size(); i++){
-			print(solutions.get(i));
-			System.out.println();
 			drawSolution(solutions.get(i));
 		}
-		//print(solutions.get(1));
-		drawSolution(num);
-		drawSolution(createReflectionAcrossYAxis(num));
-		drawSolution(createReflectionAcrossXAxis(num));
-
 	}
 	public void print(int[][] nums){
-		for(int i = 0; i < nums.length; i++)
-			System.out.println("solution: " + Arrays.toString(nums[i]));
+		for(int i = 0; i < nums.length; i++){
+			System.out.println(Arrays.toString(nums[i]));
+		}
 	}
 	
 	private boolean isSolution(int[][] array){
-		//print(array);
 		for(int i = 0; i < array.length; i++){
 			int[] firstQueen = array[i];
-			//System.out.println("FirstQueen: " + Arrays.toString(firstQueen));
 			for(int x = 0; x < array.length; x++){
 				int[] secondQueen = array[x];
-				//System.out.println("SecondQueen: " + Arrays.toString(secondQueen));
 				if(firstQueen[1] != secondQueen[1]){
 					double slope = (double)(firstQueen[1] - secondQueen[1]) / (firstQueen[0] - secondQueen[0]);
-					//System.out.println("slope: " + slope);
-					//System.out.println("FirstQueen: " + Arrays.toString(firstQueen));
-					//System.out.println("SecondQueen: " + Arrays.toString(secondQueen));
 					if (slope == 1 || slope == -1) return false;
 				}
 			}
@@ -152,11 +120,10 @@ public class EightQueens {
 	}
 	
 	private void drawSolution(int[][] solution){
-		print(solution);
-		SketchPad paper = new SketchPad(1000, 1000);
+		double scale = 2;
+		SketchPad paper = new SketchPad(80 * (int) scale + 100, 80 * (int) scale + 100);
 		DrawingTool pen = new DrawingTool(paper);
 		
-		double scale = 5;
 		
 		pen.down();
 		pen.drawRect(80 * scale, 80 * scale);
@@ -175,7 +142,6 @@ public class EightQueens {
 
 		
 		for(int i = 0; i < solution.length; i++){
-			System.out.println(solution[i][0]);
 			pen.up();
 			pen.move((solution[i][0] * 10 - 35) * scale, (solution[i][1] * 10 - 35) * -scale);
 			pen.down();
@@ -183,119 +149,132 @@ public class EightQueens {
 		}
 	}
 	
-	private int[][] copyArray(int[][] arrayToCopy){
-		int[][] newArray = arrayToCopy.clone();
+	private void createNonAnswers(int[][] array){
+		int[][] one = createRotation(array);
+		int[][] two = createRotation(one);
+		int[][] three = createRotation(two);
+		nonSolutions.add(array);
+		nonSolutions.add(one);
+		nonSolutions.add(two);
+		nonSolutions.add(three);
+		
+		for(int i = 0; i < 4; i++){
+			if(i == 0){
+				int[][] yReflection = createReflection(array);
+				nonSolutions.add(yReflection);
+			}
+			if(i == 1){
+				int[][] yReflection = createReflection(one);
+				nonSolutions.add(yReflection);
+			}
+			if(i == 2){
+				int[][] yReflection = createReflection(two);
+				nonSolutions.add(yReflection);
+			}
+			if(i == 3){
+				int[][] yReflection = createReflection(three);
+				nonSolutions.add(yReflection);
+			}
+		}
+	}
+	
+	private int[][] createReflection(int[][] array){
+		int[][] newArray = new int[8][2];
+		for(int i = 7; i >= 0; i--){
+			double xDistance = 3.5 - array[i][0];
+			double newX = xDistance + 3.5;
+			newArray[i][0] = (int) newX;
+			newArray[i][1] = i;
+		}
 		return newArray;
 	}
 	
+	private int[][] createRotation(int[][] array){
+		int[][] newArray = new int[8][2];
+		for(int i = 7; i >= 0; i--){
+			for(int x = 0; x < array.length; x++){
+				if(array[x][0] == i){
+					newArray[Math.abs(array[x][0] - 7)][0] = array[x][1];
+					newArray[Math.abs(array[x][0] - 7)][1] = Math.abs(array[x][0] - 7);
+				}
+			}
+		}
+		return newArray;
+	}
+	
+	private boolean isSolutionChosenBefore(int[][] queenPositions){
+		for(int i = 0; i < nonSolutions.size(); i++){
+			if(Arrays.deepEquals(nonSolutions.get(i), queenPositions)){
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 	private int[][] createReflectionAcrossXAxis(int[][] array){
 		int[][] newArray = new int[8][2];
-		for(int i = 0; i < array.length; i++){
+		for(int i = 7; i > 0; i--){
 			double xDistance = 3.5 - array[i][0];
 			double yDistance = 3.5 - array[i][1];
 			double newX = xDistance + 3.5;
 			double newY = yDistance + 3.5;
-			newArray[i][0] = (int) array[i][0];
+			newArray[i][0] = array[i][0];
 			newArray[i][1] = (int) newY;
 		}
 		return newArray;
 	}
 	
-	private int[][] createReflectionAcrossYAxis(int[][] array){
-		int[][] newArray = new int[8][2];
-		for(int i = 0; i < array.length; i++){
-			double xDistance = 3.5 - array[i][0];
-			double yDistance = 3.5 - array[i][1];
-			double newX = xDistance + 3.5;
-			double newY = yDistance + 3.5;
-			newArray[i][0] = (int) newX;
-			newArray[i][1] = (int) array[i][1];
-		}
-		return newArray;
-	}
 	
-	private void createRotation(int[][] array){
-		
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-for(int first = 0; first < 7; first++){
-	if(!temp.contains((Integer) first))temp.add((Integer) first);
-
-	for(int second = 0; second < 7; second++){
-		if(!temp.contains((Integer) second))temp.add((Integer) second);
-		
-		for(int third = 0; third < 7; third++){
-			if(!temp.contains((Integer) third))temp.add((Integer) third);
-			
-			for(int tempIndex = 0; tempIndex < temp.size(); tempIndex++){
-				queenPositions[0][0] = first;
-				queenPositions[1][0] = second;
-				queenPositions[2][0] = third;
-				print(queenPositions);
-				temp.clear();
-			}
-		}
-	}
-}
-*/
-
-/*
-for (int i = 0; i < 7; i++){
-	if(!temp.contains((Integer) i))temp.add((Integer) i);
-}
-*/
-
-
-
-/*
- * 
-
-for (int i = 0; i < 8; i++){
-	queenPositions[i][1] = i;
-}
-print(queenPositions);
-
-
-queenPositions[0][0] = first;
-queenPositions[1][0] = second;
-queenPositions[2][0] = third;
-queenPositions[3][0] = fourth;
-queenPositions[4][0] = fifth;
-queenPositions[5][0] = sixth;
-queenPositions[6][0] = seventh;
-queenPositions[7][0] = eigth;
-print(queenPositions);
-System.out.println("temp: " + temp);
-temp.remove(temp.size() - 1);
+	int[][] num = {
+				{6, 0},
+				{4, 1},
+				{2, 2},
+				{0, 3},
+				{5, 4},
+				{7, 5},
+				{1, 6},	
+				{3, 7}
+		};
+		int[][] num2 = {
+				{4, 1},
+				{6, 0},
+				{2, 2},
+				{0, 3},
+				{5, 4},
+				{7, 5},
+				{1, 6},	
+				{3, 7}
+		};
 */
